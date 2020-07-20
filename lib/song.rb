9efeb45fdb70e5e99b94d8d9d7d0c94d2cbde3dd
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-20 16:01:45 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+require 'pry'
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artist_count = {}
+  @@genre_count = {}
+
+  def initialize (name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+  end
+
+  
+
+end
